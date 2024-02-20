f1538ccebc7d394bac4ad07d0660a5f5e68ec25f
naleilan <leilan.n@gmail.com>
2024-02-20 21:33:55 +0300

    add base

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 function App() {
-  return <div>App</div>;
+  return (
+    <main>
+      <section className="left">
+        <p className="loader1"></p>
+      </section>
+      <section className="right">
+        <form>
+          <section className="top">
+            <h1>title</h1>
+            <p>text</p>
+          </section>
+          <section className="buttons">
+            <button>back</button>
+            <button>next</button>
+          </section>
+        </form>
+      </section>
+    </main>
+  );
 }
 
 export default App;
