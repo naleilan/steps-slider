6413f491afc398f12a24767c32334beb8da97426
naleilan <leilan.n@gmail.com>
2024-02-21 10:56:39 +0300

    compelete css

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,40 @@
 function App() {
   return (
     <main>
-      <section className="left">
-        <p className="loader1"></p>
-      </section>
-      <section className="right">
-        <form>
-          <section className="top">
-            <h1>title</h1>
-            <p>text</p>
-          </section>
-          <section className="buttons">
-            <button>back</button>
-            <button>next</button>
-          </section>
-        </form>
-      </section>
+      <Shape />
+      <Main />
     </main>
+  );
+}
+
+function Shape() {
+  return (
+    <section className="left">
+      <p className="loader1">1</p>
+      <p className="loader2">2</p>
+      <p className="loader3">3</p>
+    </section>
+  );
+}
+
+function Main() {
+  return (
+    <section className="right">
+      <form>
+        <section className="top">
+          <h1>Side Slider Sample</h1>
+          <p className="text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed
+            laudantium consectetur quasi, quo libero voluptas dolore placeat
+            quas, dolorum ipsa perferendis?
+          </p>
+        </section>
+        <section className="buttons">
+          <button className="btn btn-purple">&#8249;</button>
+          <button className="btn btn-purple">&#8250;</button>
+        </section>
+      </form>
+    </section>
   );
 }
 
